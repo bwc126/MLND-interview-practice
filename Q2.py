a940e88c4fdd06ad6c5dc9a713dcbaf55dc4b5ec
Brian Coe <bwc126@gmail.com>
2017-02-04 14:32:22 -0500

    feat: Improve Readme, scaffold Q2

---
+++ Q2.py
@@ -0,0 +1,8 @@
+# Given a string a, find the longest palindromic substring contained in a. Your function definition should look like question2(a), and return a string.
+
+def question2(a):
+# Steps might be:
+
+# Split the string in half, check if the reverse of this half is in the rest of the string
+
+# Do this recursively and iteratively with successively smaller substrings at different frames within the string until we've exahusted the possibilities for two letters
