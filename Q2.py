fa463deac13935aaa6932c46df0711206262ed07
Brian Coe <bwc126@gmail.com>
2017-02-07 19:39:25 -0500

    feat: Begin revising to recursive structure

--- Q2.py
+++ Q2.py
@@ -6,18 +6,21 @@ def question2(a):
 # Steps might be:
 
     # Split the string in half, check if the reverse of this half is in the rest of the string
-    stop = int(math.floor(len(a)/2))
-    print stop
-    b = a[0:stop]
-    c = b[::-1]
-    d = b + c
-    e = c + b
-    print b, c, d, e
-    if d in a:
-        return d
-    if e in a:
-        return e
+    def test(s):
+        stop = int(math.floor(len(a)/2))
+        print stop
+        b = s[0:stop]
+        c = b[::-1]
+        d = b + c
+        e = c + b
+        print b, c, d, e
+        if d in a:
+            return d
+        if e in a:
+            return e
     # Do this recursively and iteratively with successively smaller substrings at different frames within the string until we've exahusted the possibilities for two letters
+    while len(a) > 2:
+        test(a)
 
 print question2('abab')
 print question2('avva')
