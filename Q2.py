041738399377db74e0f78e984e3ef2de043f4c4a
Brian Coe <bwc126@gmail.com>
2017-02-08 23:02:05 -0500

    feat: Improve solution for consistency, still has off-by-one issues

--- Q2.py
+++ Q2.py
@@ -7,24 +7,34 @@ def question2(a):
 
     # Split the string in half, check if the reverse of this half is in the rest of the string
     def test(s, offset, width):
-        # stop = int(math.floor(len(s)/2))
+        stop = int(math.floor(width/2))
         # print stop
-        b = s[0+offset:width+offset]
-        c = b[::-1]
-        d = b + c
-        e = c + b
-        print b, c, d, e
-        if d in s:
-            return d
-        if e in s:
-            return e
+        b = s[0+offset:stop+offset]
+        c = s[stop+offset:width+offset]
+        # print b, c
+        if b == c[::-1]:
+            return b+c
+        else:
+            return ''
+        # d = b + c
+        # e = c + b
+        # print b, c, d, e
+        # if d in s:
+        #     return d
+        # if e in s:
+        #     return e
     # Do this recursively and iteratively with successively smaller substrings at different frames within the string until we've exahusted the possibilities for two letters
     result = ''
-    width = int(math.floor(len(a)/2))
+    width = len(a)
+    old_result = ''
+    new_result = ''
     while width >= 2:
-        rec = len(a) - (2 * width) + 1
-        for it in range(rec+1):
-            result = test(a, it, width)
+        offsets = len(a) - width + 1
+        for offset in range(offsets):
+            new_result = test(a, offset, width)
+        if len(new_result) < len(old_result):
+            return old_result
+        old_result = new_result
         width -= 1
     return result
 print question2('abab')
