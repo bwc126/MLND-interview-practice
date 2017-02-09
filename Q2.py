8e5e6bd874fafc6556f6ba501f2248b610bb0878
Brian Coe <bwc126@gmail.com>
2017-02-08 22:08:19 -0500

    feat: Attempt improvement to Q2

--- Q2.py
+++ Q2.py
@@ -6,22 +6,28 @@ def question2(a):
 # Steps might be:
 
     # Split the string in half, check if the reverse of this half is in the rest of the string
-    def test(s):
-        stop = int(math.floor(len(a)/2))
-        print stop
-        b = s[0:stop]
+    def test(s, offset, width):
+        # stop = int(math.floor(len(s)/2))
+        # print stop
+        b = s[0+offset:width+offset]
         c = b[::-1]
         d = b + c
         e = c + b
         print b, c, d, e
-        if d in a:
+        if d in s:
             return d
-        if e in a:
+        if e in s:
             return e
     # Do this recursively and iteratively with successively smaller substrings at different frames within the string until we've exahusted the possibilities for two letters
-    while len(a) > 2:
-        test(a)
-
+    result = ''
+    width = int(math.floor(len(a)/2))
+    while width >= 2:
+        rec = len(a) - (2 * width) + 1
+        for it in range(rec+1):
+            result = test(a, it, width)
+        width -= 1
+    return result
 print question2('abab')
 print question2('avva')
 print question2('avcva')
+print question2('ls;dnfuewhrojaaajjaaaa;sdlkfnlk')
