3089a3e82942a274a2d9c9f1fa6941b6375415f7
Brian Coe <bwc126@gmail.com>
2017-02-12 22:38:49 -0500

    feat: Improve Q1, Q4

--- Q1.py
+++ Q1.py
@@ -1,7 +1,9 @@
 # Given two strings s and t, determine whether some anagram of t is a substring of s. For example: if s = "udacity" and t = "ad", then the function returns True. Your function definition should look like: question1(s, t) and return a boolean True or False.
 
 def question1(s,t):
-    # We can replace within s each character that's in t with some placeholder character (hyphen in this example), and then check to see if there's a string of placeholder characters within s that correspond to the length of t. Since we want all possible anagrams to be covered, any of the characters in t can be in any position within s, and as long as they're adjacent and there are at least as many in s as there are in t, we can confidently declare some anagram of t is within s.
+    # We can replace within s each character that's in t with some placeholder character (hyphen in this example), and then check to see if there's a string of placeholder characters within s that correspond to the length of t. Since we want all possible anagrams of t to be covered, any of the characters in t can be in any position within s, and as long as they're adjacent in s and there are at least as many in s as there are in t, we can confidently declare some anagram of t is within s.
+    if not s or not t:
+        return
     test = ''
     for char in t:
         s = s.replace(char, '-')
@@ -17,3 +19,4 @@ print question1('udacity','ad') # Should be True
 print question1('udacity','ciud') # Should be False
 print question1('udacity','uy') # Should be False
 print question1('sarsaparilla','apas') # Should be True
+print question1('','') # Should be None
--- Q4.py
+++ Q4.py
@@ -10,3 +10,75 @@
 #           4)
 #
 # and the answer would be 3.
+
+# The matrix contains a number of rows equal to one plus the highest number in the BST. The index of a list within the matrix corresponds to each node's value. If the list for a node contains a 1, that node has a child whose value is the index of the position of the 1 within the list. For the example given above, the BST would have a 3 at the root, 0 on the left, 4 on the right. The 0 would have a child of 1 on the right.
+
+# The signature question4(T, r, n1, n2) includes T, a binary matrix as described previously, r, a non-neg integer corresponding to the value of the root. n1, n2 are each non-neg ints representing the two nodes for which we need to find the greatest common ancestor node. We can assume n1, n2 might be in any order, that both nodes are in fact within the tree, and the BST conforms to standard rules for a BST.
+import copy
+def question4(T, r, n1, n2):
+
+    # Since r is the value of the root, we should look within T and find the list at that index value to see what children our root has. Ultimately, we need to find n1, n2's parents, and see if they're actually the same node, and if not, how far up the tree we need to look to find the greatest common ancestor for them. If the root is the immediate parent of n1,n2, then we've found our result. Otherwise, we need to keep moving up the tree until we find a parent node both n1,n2 have in common.
+
+    # Start by discarding trivial rows, storing the remaining rows with their node value in a dictionary as a key, and a list of their children as values. 0: [1] would be one such dictionary entry for the example in the question definition.
+    nodes = {}
+    # print T
+    M = copy.deepcopy(T)
+    for row in range(len(M)):
+        if 1 in M[row]:
+            children = []
+            for child in range(M[row].count(1)):
+                loc = M[row].index(1)
+                children.append(loc)
+                M[row][loc] = 0
+            nodes[row] = children
+    # print nodes
+    # print T == M
+    # We then recursively find all parents for each of n1, n2 all the way to the root.
+    def find_ancestors(val, ancestors, tree):
+        # print val, ancestors, tree
+        if val == r:
+            return ancestors
+        else:
+            for node in tree:
+                if val in tree[node]:
+                    ancestors.append(node)
+                    return find_ancestors(node, ancestors, tree)
+
+
+    a_1 = []
+    a_2 = []
+    a_1 = find_ancestors(n1, a_1, nodes)
+    a_2 = find_ancestors(n2, a_2, nodes)
+    # print a_1, a_2
+
+    # Compare the lists of ancestors. We should return the first one that appears in both. We 'for' loop through one, check if it's in the other, and if it is, we return that value immediately, ending the function qeustion4().
+    for ancestor in a_1:
+        if ancestor in a_2:
+            return ancestor
+
+
+
+
+# Test Cases
+matrix = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # 0
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,1,0,0,0,0,1,0,0,0,0,0,0,0,0], # 3
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,1,0,0,1,0,0,0,0,0,0,0], # 6
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # 9
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # 12
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]]
+root = 8
+n_1, n_2 = 4, 7
+print question4(matrix, root, n_1, n_2) # Should be 6
+n_3, n_4 = 1, 6
+print question4(matrix, root, n_3, n_4) # Should be 3
+n_5, n_6 = 4, 10
+print question4(matrix, root, n_5, n_6) # Should be 8
